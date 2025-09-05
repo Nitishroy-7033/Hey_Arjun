7e38d9922f444d920450bf4b922f40cb89408ae0
Nitish Roy <Nitishr833@gmail.com>
2025-09-06 00:40:04 +0530

    Refine listening prompts and adjust pause threshold for improved voice recognition

--- core/listener.py
+++ core/listener.py
@@ -3,15 +3,15 @@ import speech_recognition as sr
 def listen_voice():
     recoginizer = sr.Recognizer()
     mic = sr.Microphone()
-    recoginizer.pause_threshold = 3  
+    recoginizer.pause_threshold = 2
     with mic as source:
-        print("🎤 Jarvis is listening...")
+        print("Adjusting for ambient noise, please wait...")
         recoginizer.adjust_for_ambient_noise(source,duration=2)
         audio = recoginizer.listen(source)
 
         while True:
             try:
-                print("👂 Listening...")
+                print("👂 Jarvis listening...")
                 audio = recoginizer.listen(source)  # will stop after 3 sec silence
 
                 # Process speech
